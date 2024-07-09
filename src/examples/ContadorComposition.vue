de2110dad7bfa7e482ace8fd29b78258274565b4
Luis Quintero <quinteropartidaluis@gmail.com>
2024-07-09 11:34:35 -0600

    Basic examples

---
+++ src/examples/ContadorComposition.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+    import { onMounted, ref } from 'vue'
+
+    const count = ref(0);
+
+    function increase() {
+        count.value += 1;
+    }
+
+    function decrease() {
+        if(count.value > 0){
+            count.value -= 1;
+        }
+        
+    }
+
+    onMounted( () => {
+        console.log('El valor inicial del contador es ' + count.value);
+    } );
+</script>
+
+<template>
+    <p>Composition API version of vue</p>
+    <h1>Contador: {{ count }}</h1>
+
+    <button @click="increase">
+        Incrementar contador
+    </button>
+    <button @click="decrease">
+        Disminuir contador
+    </button>
+
+</template>
